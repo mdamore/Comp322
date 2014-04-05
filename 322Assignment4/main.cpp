de653f4c0bfb1e8c8369aa28e454330439043d97
mdamore <mdamore9@gmail.com>
2014-04-05 09:39:09 -0400

    Initial Commit

---
+++ 322Assignment4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  322Assignment4
+//
+//  Created by Marco d'Amore on 4/5/14.
+//  Copyright (c) 2014 Marco d'Amore. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
